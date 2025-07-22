aa5ba086e1667ea3a113bfb8b622271d5c37feb5
lalaamy <lalaamy@naver.com>
2025-07-22 17:38:07 +0900

    z

--- baekjoon1/1차 배열/최댓값.py
+++ baekjoon1/1차 배열/최댓값.py
@@ -4,8 +4,8 @@ numbers = []
 for n in range(9) :
     num = int(input())
     numbers.append(num)
-    
-    M = max(numbers)
+
+M = max(numbers)
     
 print(M)
-print(numbers.index(M))
+print(numbers.index(M)+1)
